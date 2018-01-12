c69552de042156d32fc15337b42c84b978a9b204
nathan-wiegman <34677572+nathan-wiegman@users.noreply.github.com>
2018-01-11 21:31:37 -0500

    Completed simulation
    Completed simulation and tested with fake data. Appears to match with prior excel simulations.
    - Plots - battery power
                - SOC
                - Voc and Vterm

--- thesis.py
+++ thesis.py
@@ -14,7 +14,7 @@ drive system effectiveness, mission energy, and battery utilization
     nicefig(name, w,h,**kwargs)         Cleans up plot 
 # ==========================================================="""
 
-def missionrange(eta, mass, omega, torque, name='Mission0.png', title='Baseline Power Profile'):
+def missionrange(M, name='Mission0.png', title='Baseline Power Profile'):
     """## =========== Mission Range function ======================================
 This function determines the total mission range capability given 
 a certain motor efficiency and mass. The other aspects are as follows:
@@ -26,17 +26,7 @@ but these aspects are considered system constants and are not changed
 for simplicity. 
 
 INPUTS: 
-    eta     NP.MATRIX   Motor efficiency within range 0 -> 1
-                        n x m matrix
-
-    mass    FLOAT       Motor mass in kg
-
-    omega   FLOAT       Range of motor rotational speeds
-                        n x 1 vector
-
-    torque  FLOAT       Range of motor torques
-                        m x 1 vector
-   
+    M       MISSION Class with info from battrange(9
 OUTPUTS:
     range   FLOAT   Mission range in nm
 ## =============================================================================="""
@@ -48,25 +38,12 @@ OUTPUTS:
     import pandas as pd
     from scipy import integrate
 
-    # Mission class is defined below. Contains constants of mission power, duration, etc
-    M = th.mission()
-    
     # Find value of non-cruise energy and distance
-    power, time, speed = missionenergy(M)
-     
-    energy0 = np.trapz(power, time)/3600    # [kWh] Total energy of non-cruise 
-    dist0 = np.trapz(speed, time)/3600      # [km]  Total dist non-cruise
+    energy0 = np.trapz(M.power, M.time)/3600    # [kWh] Total energy of non-cruise 
+    dist0 = np.trapz(M.speed, M.time)/3600      # [km]  Total dist non-cruise
    
     print("The energy usage in the non-cruise portions is {0:0.2f} [kWh]".format(energy0))
     print("                 and the distance traveled is {0:.2f} [km].".format(dist0))
-
-    # Find battery mass
-    battMass = EPPSmass - mass  # [kg] total EPPS mass minus motor system mass
-    
-    # Flight Power Plot
-    T_CRUISE = 2692         # [s]   Assumed from initial trace, edit 
-                            # depending on final plot
-    M.T[2] = T_CRUISE
 
     time = range(0,60+sum(M.T))
 
@@ -130,17 +107,26 @@ Creates a 3D plot to display variations in range given many possible motor desig
 
 # End heatrange() =================================================================
 
-def battrange(eta,mass,omega,torque):
+def battrange(eta,mass,omega,torque,**kwargs):
     """## ======== Battery Range function ========================================
 Function that takes known battery params and simulates the battery response given a
 specific motor efficiency. Assumes a full battery and ideal mission conditions.
 
+Uses th.mission and th.cell classes. Reference th.mission.__doc__ or th.cell.__doc__
+for more information.
+
 #Inputs: 
-    eta     NP.MATRIX   n by m 2D matrix of efficiency data for the motor. MUST CORRESPOND TO omega AND torque in range of (0,1)
+    eta     NP.MATRIX   n by m 2D matrix of efficiency data for the motor. MUST CORRESPOND TO omega AND torque.
+                                    In range of (0,1)
     mass    FLOAT       Motor system mass, used to calculate battery mass
     omega   LIST        n long list of motor output rotational speed [rad/s]
+                        Solely used for plotting, not relevant for power calculations
     torque  LIST        m long list of motor output torques [Nm]
-
+                        Solely used for plotting, not relevant for power calculations
+    **filename STRING   Optional output parameter to change filename, set type by 
+                                    suffix. Supports eps, pdf, png, svg, and a few 
+                                    others. See matplotlib.pyplot.savefig() 
+                                    documentation.
 #Outputs:
     range   FLOAT       Maximum theoretical range of the system
 
@@ -151,6 +137,7 @@ specific motor efficiency. Assumes a full battery and ideal mission conditions.
     from scipy import integrate
     import scipy.interpolate
     import thesis as th
+    import matplotlib.pyplot as plt
 
     ## System contants, see th.mission.__doc__ for detailed info
     M = th.mission()
@@ -161,24 +148,160 @@ specific motor efficiency. Assumes a full battery and ideal mission conditions.
     # Battery info, see th.cell.__doc__ for detailed info
     C = th.cell(battMass)
     
-    PProp = scipy.interpolate.interp1d(M.time, M.power)
-    
-    ## Create simulation
-    # time is different from M.time. Longer to account for increased efficiency.
-    time = np.linspace(0,5000,5001)     # 1s timestep
-   
-    for t in time:
-        # fetch prop power and RPM from P and RPM lists
-        # throw error if prop power is higher than motor capability
-        # fetch motor system eta 
-        # calculate battery power draw from system eta
-        pass
+    # interpolate the eta matrix for use in sim
+    etaInterp = scipy.interpolate.interp2d(omega,torque,eta)
 
+    ## Create simulation
+    # initialize values to hold sim outputs
+    S = th.simData(C)
+
+    # Set cruise length flag
+    landingPower = True
+
+    while (S.t < 30 + M.T[0]): # do TO (hover)
+        S = sim(M,C,S,etaInterp,0)         
+        Pmax = (C.Voc(S.SOC[S.t])-C.Vmin)/C.Res(S.SOC[S.t])*C.Vmin/1000 # [kW]
     
+    while (S.t < (30 + sum(M.T[0:2]))): # do phase 2 (climb)
+        S = sim(M,C,S,etaInterp,1)
+        Pmax = (C.Voc(S.SOC[S.t])-C.Vmin)/C.Res(S.SOC[S.t])*C.Vmin/1000 # [kW]
+
+    # while cruise THIS ONE IS VARIABLE LENGTH
+    while landingPower is True:
+        S = sim(M,C,S,etaInterp,2) # returns tCruise
+        tCruise = S.t - 30 - sum(M.T[0:2])
+        # Calculate current max possible power draw and set flag if under requirements
+        Pmax = (C.Voc(S.SOC[S.t])-C.Vmin)/C.Res(S.SOC[S.t])*C.Vmin/1000 # [kW]
+        if Pmax < M.power[2]:
+            landingPower = False
+            S.removeLast()  # Removes elements that would place landing reqs over max batt output     
+    print("Cruise duration is",tCruise,"seconds")
+    print("Current time is", S.t)
+
+    # while desc,slow,stop,tran,land
+    for x in range(3,8):
+        while S.t < (30 + sum(M.T[0:x+1]) + tCruise):
+            S = sim(M,C,S,etaInterp,x)
+    
+    S.finalValues()
+    
+    print("Simulation complete!")
+    
+    if "filename" in kwargs:
+        S.plot(C,kwargs["filename"])
+    else:
+        S.plot(C,"SimulationResults.png")
+    return S
 # End battrange() ====================================================================
+
+def sim(M,cell,S,eta,phase):
+    import numpy as np
+    
+    battPower = S.battPower
+    SOC = S.SOC
+    current = S.current
+    t = S.t
+
+    S.battPower.append(M.P[phase] / eta(M.omega[phase],M.torque[phase])) # may need to swap index order
+    
+    S.current.append((cell.Voc(SOC[t]) - np.sqrt(max(0.01, cell.Voc(SOC[t])**2-4*cell.Res(SOC[t])*battPower[t]*1000)))/(2*cell.Res(SOC[t])))
+    
+    S.Vterm.append(cell.Voc(SOC[t]) - S.current[t] * cell.Res(SOC[t]))
+    S.SOC.append(max(0, min(1,SOC[t] - np.trapz([float(current[t-1]),float(current[t])],np.divide([t-1,t],3600))/cell.packCap)))
+          
+    S.t += 1
+    S.time.append(t)
+    
+    return S
+
+# End sim() ==========================================================================
+    
+
+class simData():
+    """##============ simData ==========================================================
+Class that contains simulation results. Used as single argument to sim function.
+
+#FIELDS:
+    battpower   LIST    contains the power drawn from the battery
+    current     LIST    contains the current drawn from the battery
+    SOC         LIST    contains the SOC of the battery
+    t           INT     contains the present time of the simulation
+    time        LIST    contains the overall time vector
+    Vterm       LIST    contains the terminal voltage of the battery
+
+#METHODS:
+    removeLast()        removes the last element of all LIST fields
+                        and updates t to the last timestep
+    plot(cell)          plots simulation data
+    finalValues()       appends final values to complete plot       
+"""
+    def __init__(self,cell):
+        self.battPower = [0] * 60
+        self.current = [0] * 60 
+        self.SOC = [1] * 60
+        self.t = 59
+        self.time = list(range(60))
+        self.Vterm = [cell.Voc(1)] * 60
+
+    def removeLast(self):
+        """##========= removeLast Method ===============================================
+Method that resets to the previous time step."""
+        self.battPower = self.battPower[:-1]
+        self.current = self.current[:-1]
+        self.SOC = self.SOC[:-1]
+        self.time = self.time[:-1]
+        self.t -= 1
+        self.Vterm = self.Vterm[:-1]
+
+    def plot(self,cell,filename):
+        """##=========== simPlot Method =================================================
+Method that plots the contents of the simData class. 
+
+#INPUTS:
+    cell    OBJECT  class object that contains the cell parameters
+"""
+        import matplotlib.pyplot as plt
+        import thesis as th
+        from matplotlib.offsetbox import TextArea, VPacker, AnchoredOffsetbox, HPacker, VPacker
+
+        fig, ax1 = plt.subplots()
+        ax2 = ax1.twinx()
+        l1 = ax1.plot(self.time,self.battPower,'b',label="P_{batt}")
+        l2 = ax1.plot(self.time,cell.Voc(self.SOC),'g:',label="V_{oc}")
+        l3 = ax1.plot(self.time,self.Vterm,'g',label="V_{term}")
+        l4 = ax2.plot(self.time,self.SOC,'r',label="SOC")
+        ax2.set_ylim([0,1])
+        ax1.set_xlim([0,max(self.time)])
+        ax1.set_xlabel('Time [s]')
+        
+        # Draw multicolored axis label
+        ybox1 = TextArea("Battery Power [kW]", textprops=dict(color='b',size=10, rotation=90,ha='left',va='bottom'))
+        ybox2 = TextArea("Voltage [V]", textprops=dict(color='g',size=10, rotation=90,ha='left',va='bottom'))
+        ybox = VPacker(children=[ybox1,ybox2],align="right",pad=5,sep=10)
+        anchored_ybox = AnchoredOffsetbox(loc=8,child=ybox,pad=0.,frameon=False,bbox_to_anchor=(-0.08,0.2),bbox_transform=ax1.transAxes, borderpad=0.)
+        ax1.add_artist(anchored_ybox)   
+
+        ax2.set_ylabel('State of Charge [-]',color='r')
+
+        lines = l1 + l2 + l3 + l4
+        labels = [l.get_label() for l in lines]
+        plt.legend(lines, labels, loc=8)
+
+        th.nicefig(filename,title="Mission Simulation Results") 
+        
+    def finalValues(self):
+        for x in range(60):
+            self.battPower.append(0)
+            self.current.append(0)
+            self.SOC.append(self.SOC[-1])
+            self.time.append(max(self.time)+1)
+            self.Vterm.append(self.Vterm[-1])
+
+## End sim class ========================================================================
 class cell():
-    """#=========== cell() class =======================================================
+    """#=========== cell(battMass = 250) class ===============================================
 Class that contains the constant values for the battery cell, and some derived values.
+Default battery mass of 250 [kg] corresponds to 2 packs of 195 series by 13 parallel cells.
 
 #FILEDS:
     #Constants:
@@ -189,6 +312,7 @@ Class that contains the constant values for the battery cell, and some derived v
         cellRes     LIST of cell resistances at corresponding SOC   [Ohm]
         cellVoc     LIST of cell open-circuit voltages at SOC       [V]
         cellCap     cell capacity                                   [Ah]
+        Vmin        Minimum pulse voltage                           [V]
 
     #Derived Values:
         noCells     number of cells in pack                         [-]
@@ -200,13 +324,13 @@ Class that contains the constant values for the battery cell, and some derived v
         Voc         Scipy interpolation. Pack open circuit voltage. [V]
                     Call this as cell.Voc(SOC)
 """
-    def __init__(self,battMass):
+    def __init__(self,battMass = 250): # default of 250kg. 195s13p pack x2 
         import pandas as pd
         import numpy as np
         import scipy.interpolate
         # import data
         self.LGMJ1 = pd.read_csv('/Users/Asmodeus/Documents/Danmarks_Tekniske_Universitet/S161568MSC/LGMJ1.csv', header=None,delimiter = ";") # Pandas dataframe
-        self.CELLS_S = 195           # [-]
+        self.CELLS_S = 195              # [-]
         self.cellMass = float(self.LGMJ1[1][7].replace(',','.')) # [kg] Gets mass from dataframe and changes to decimal radix
 
         # Approximate cells in parallel
@@ -219,7 +343,7 @@ Class that contains the constant values for the battery cell, and some derived v
         self.packNomEnergy = self.CELLS_S*float(self.LGMJ1[1][12].replace(',','.'))*self.packCap/1000 # [kWh] NOMINAL
     
         ## Interpolate Resistance and Voc 
-        self.SOCinit = [100,90,80,70,60,50,40,30,20,10,0]    # initial data is every 10% 
+        self.SOCinit = [1,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0]    # initial data is every 10% 
 
         # Preallocate
         self.cellRes = [0]*len(self.SOCinit)
@@ -235,6 +359,9 @@ Class that contains the constant values for the battery cell, and some derived v
         #  interpolate
         self.Res = scipy.interpolate.interp1d(self.SOCinit,self.Res)   # call this as Res(SOC)
         self.Voc = scipy.interpolate.interp1d(self.SOCinit,self.Voc)
+
+        # pack minimum
+        self.Vmin = 2.6 * self.CELLS_S  # [V]
 
 class mission():
     """#============ mission() class ====================================================
@@ -285,6 +412,7 @@ Class that contains the constant values of the mission, and some derived values.
     S = [0,   climbS, cruiseS, descS,0,     0,      0,     0]
                     assume negligible groundspeed during hover portions
     RPM = [RPM_TO,RPM_CLIMB,RPM_CRUISE,RPM_DESC,RPM_SLOW,RPM_STOP,RPM_LAND]
+    omega           LIST is equivalent to RPM, converted to     [rad/s]
     time            LIST of times 1s time step, 60s longer than sum of T
                     30s header and 30s tail
     power           LIST of powers at 1s time step
@@ -292,10 +420,13 @@ Class that contains the constant values of the mission, and some derived values.
     rotspeed        LIST of rotor rotational speeds at 1s time step
     """
     def __init__(self):    # System Constants
-        self.NM_TO_KM = 1.85         # [km/NM]
-        self.CRUISE_SPEED = 110      # [NM/h]
-        self.CLIMB_SPEED = 80        # [NM/h]
-        self.DESC_SPEED = 100        # [NM/h]
+        import numpy as np
+        import scipy.interpolate
+        
+        self.NM_TO_KM = 1.85         # [km/NMi]
+        self.CRUISE_SPEED = 110      # [NMi/h]
+        self.CLIMB_SPEED = 80        # [NMi/h]
+        self.DESC_SPEED = 100        # [NMi/h]
 
         self.VMTOW = 1633            # [kg]
         self.STRUCT_FRAC = 1/3       # [-]    
@@ -321,7 +452,7 @@ Class that contains the constant values of the mission, and some derived values.
         self.RPM_TO = 1200           # [RPM]
         self.RPM_CLIMB = 1050        # [RPM]
         self.RPM_CRUISE = 1000       # [RPM]
-        self.RPM_DESC = 850           # [RPM]
+        self.RPM_DESC = 850          # [RPM]
         self.RPM_SLOW = 1100         # [RPM]
         self.RPM_TRAN = 1000         # [RPM]
         self.RPM_STOP = 1200         # [RPM]
@@ -338,11 +469,14 @@ Class that contains the constant values of the mission, and some derived values.
         self.T = [self.T_TO,self.T_CLIMB, 0,self.T_DESC,self.T_SLOW,self.T_TRAN,self.T_STOP,self.T_LAND]
         self.S = [0,   self.climbS, self.cruiseS, self.descS,0,     0,      0,      0]
         self.RPM=[self.RPM_TO,self.RPM_CLIMB,self.RPM_CRUISE,self.RPM_DESC,self.RPM_SLOW,self.RPM_TRAN,self.RPM_STOP,self.RPM_LAND]
-        
+        self.omega = np.multiply(np.divide(self.RPM,60),2*np.pi)
+        self.Torque = np.divide(np.multiply(self.P,1000),self.omega)
+
         time = range(0,sum(self.T)) 
         power = [0] * (max(time)+1) # one second time step
         speed = [0] * (max(time)+1)
         rotspeed = [0] * (max(time)+1)
+        torque = [0] * (max(time)+1)
 
         for x in range(len(self.P)):
             if x == 0:
@@ -350,18 +484,22 @@ Class that contains the constant values of the mission, and some derived values.
                 for y in range(self.T[x]):
                     power[y] = self.P[x]
                     speed[y] = self.S[x]
-                    rotspeed[y] = self.RPM[x]
+                    rotspeed[y] = self.omega[x]
+                    torque[y] = self.Torque[x]
             else:
                 for y in range(tlast,tlast+self.T[x]):
                     power[y] = self.P[x]
                     speed[y] = self.S[x]
-                    rotspeed[y] = self.RPM[x]
+                    rotspeed[y] = self.omega[x]
+                    torque[y] = self.Torque[x]
                 tlast += self.T[x]
         
+        self.torque = torque
         self.time = time
         self.power = power
         self.speed = speed
         self.rotspeed = rotspeed
+        self.PProp = scipy.interpolate.interp1d(self.time, self.power)
     
 # End class mission() =========================================================================
 
@@ -396,7 +534,7 @@ def nicefig(name, w=18, h=10,**kwargs):
     # Sets up kwarg default values, or input values 
     # if kwargs exist
     prop = {} # Empty dict 
-    for x in ['title', 'xlabel','ylabel','filepath','fignum']:
+    for x in ['title', 'xlabel','ylabel','filepath','fignum','legend']:
         if x in kwargs:
             prop[x] = kwargs[x]
         elif x == 'filepath' and x not in kwargs:
@@ -442,7 +580,10 @@ def nicefig(name, w=18, h=10,**kwargs):
     
     # add grid
     plt.grid(True)
-    
+
+    if prop["legend"] is True: 
+        plt.legend(loc=0)
+        print("Legend: On")
     # show figure
     plt.draw()
     plt.show()
